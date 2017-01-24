2b1390b795eeeea5ccd66a16d1e89bc43b841a31
Ben Hoffman <bah8892@g.rit.edu>
2017-01-24 12:45:43 -0500

    Made a Unity project to test the JSON stuff

---
+++ JSONTesting/Assets/_Scripts/HTTP_Request_Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+/// <summary>
+/// Author: Ben Hoffman
+/// Purpose of class: To see if I can't do a HTTP request up in here
+/// </summary>
+public class HTTP_Request_Test : MonoBehaviour {
+    // The URL of my ELK server
+    private string url = "http://192.168.137.134:9200/packetbeat-2017.01.24/_search?pretty=true";
+
+    // The output that I may get
+    private string output = "";
+
+	void Start ()
+    {
+        StartCoroutine(GetText());
+	}
+
+    IEnumerator GetText()
+    {
+        // Make a WWW object and give it the URL to my ELK stack server
+        WWW www = new WWW(url);
+        yield return www;
+
+        if(www.error == null)
+        {
+            // it worked, so set my output to a string. 
+            output = www.text;
+            Debug.Log(output);
+        }
+        else
+        {
+            // Print out the error that happened
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+}
