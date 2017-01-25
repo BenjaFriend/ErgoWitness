8f2154731b94e15a91b666e8b0a1999581186a3f
Ben Hoffman <bah8892@g.rit.edu>
2017-01-25 17:00:10 -0500

    Started the skeleton code for movement

--- JSONTesting/Assets/_Scripts/Spectator/Movement.cs
+++ JSONTesting/Assets/_Scripts/Spectator/Movement.cs
@@ -6,19 +6,41 @@ using UnityEngine;
 /// Author: Ben Hoffman
 /// purpose of class: To make a camera movement method
 /// that will allow the spectator to fly through the space 
-/// and look at each node('computer') and their connections
+/// and look at each node ('computer') and their connections
 /// </summary>
 public class Movement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
+    #region Fields
+
+    public float xMoveSpeed;
+    public float yMoveSpeed;
+    public float panSpeed;
+
+    #endregion
 	
-	// Update is called once per frame
+	/// <summary>
+    /// Check for input from the user and call the functions to calculate the movement
+    /// </summary>
 	void Update ()
     {
-		
+
 	}
+
+    /// <summary>
+    /// Author: Ben Hoffman
+    /// Purpose of method: To do the movement on this game object
+    /// </summary>
+    void Move()
+    {
+        
+    }
+    
+    /// <summary>
+    /// Author: Ben Hoffman
+    /// Purpose of method: To do the rotation on this object
+    /// </summary>
+    void Rotate()
+    {
+
+    }
 }
