79d5ce4c78afed7661be261e5ac18a68bebdbffc
Ben Hoffman <bah8892@g.rit.edu>
2017-02-10 20:51:49 -0500

    Lmao added some more stuff

--- JSONTesting/Assets/_Scripts/Spectator/Raycast_ExtraInfo.cs
+++ JSONTesting/Assets/_Scripts/Spectator/Raycast_ExtraInfo.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+/// <summary>
+/// Detect if the user is looking at a PC, and if they are then show
+/// the information of that PC in the HUD
+/// </summary>
 public class Raycast_ExtraInfo : MonoBehaviour {
+
+    public Text sourceIpText;
+    public Text sourcePortText;
+    public Text destIpText;
+    public Text destPortText;
+    public Text protoText;
 
     private RaycastHit hitInfo;
     private Ray ray;
     private GameObject target;
     public Animator anim;
     private Vector3 screenCenter;
+    private Computer compInfo;
 
     private void Start()
     {
@@ -29,17 +41,37 @@ public class Raycast_ExtraInfo : MonoBehaviour {
             {            
                 // Show the info on that particular on that computer
                 target = hitInfo.collider.gameObject;
-                target.GetComponent<Fade_UI>().FadeIn();
+                //target.GetComponent<Fade_UI>().FadeIn();
                 // Set the reticle to open
                 anim.SetBool("isLooking", true);
+
+                // Get the information on that PC and display it in my hud
+                SetValues(target.GetComponent<Computer>().sourceInfo);
+
             }
             else if (target != null)
             {
-                target.GetComponent<Fade_UI>().FadeOut();
+                //target.GetComponent<Fade_UI>().FadeOut();
                 target = null;
-                // Set the teicle to close
+                // Set the reteicle to close
                 anim.SetBool("isLooking", false);
             }
         }
+    }
+
+
+    public void SetValues(Source data)
+    {
+        sourceIpText.text = "Source IP: " + data.id_orig_h.ToString();
+        // Source port
+        sourcePortText.text = "Source Port: " + data.id_orig_p.ToString();
+
+
+        destIpText.text = "Dest. IP: " + data.id_resp_h.ToString();
+        // Dest port
+        destPortText.text = "Dest. Port: " + data.id_resp_p.ToString();
+
+        protoText.text = "Protocol: " + data.proto.ToString();
+
     }
 }
