8eaf82a846e9f8db7f61cca92172ab4b2abd0fab
Ben Hoffman <bah8892@g.rit.edu>
2017-02-15 17:10:00 -0500

    Plz commit

---
+++ JSONTesting/Assets/_Scripts/IPGrouping/IPGroup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// This represents the 
+/// </summary>
+public class IPGroup : MonoBehaviour {
+
+    private int[] groupAddress;     // This is the IP address parsed into integers, with delimeters at the periods
+    private string[] stringValues;
+    private int[] tempIntValues;        // Used for comparisons
+    public int[] GroupAddress { get { return groupAddress; } set { groupAddress = value; } }
+
+
+    public bool FitsInGroup(string ipToCheck)
+    {
+        // Return false if the IP is null
+        if (ipToCheck == null) return false;
+
+        // Split the IP address at periods first
+        stringValues = ipToCheck.Split('.');
+
+        tempIntValues = new int[stringValues.Length];
+        
+        for(int i = 0; i < stringValues.Length; i++)
+        {
+            int.TryParse(stringValues[i], out tempIntValues[i]);
+        }
+
+
+        // If the first 3 numbers are the same, then these IP's are of the same group
+
+        return false;
+    }
+}
---
+++ JSONTesting/Assets/_Scripts/IPGrouping/IPGroupManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is will manage all of my groups, so if an IP 
+/// does not fit into a group then I will make a new one
+/// </summary>
+public class IPGroupManager : MonoBehaviour {
+
+    public List<IPGroup> groups;
+
+	// Use this for initialization
+	void Start ()
+    {
+        groups = new List<IPGroup>();
+	}
+	
+    /// <summary>
+    /// Loops through and checks if this IP fits into any of these groups
+    /// If it does, then add it to the group. If not, create a new group 
+    /// based on it
+    /// </summary>
+    /// <param name="ipToCheck"></param>
+    public void CheckGroups(string ipToCheck)
+    {
+        if (ipToCheck == null) return;
+
+        for(int i = 0; i < groups.Count; i++)
+        {
+            // if this IP fits into the group
+            if (groups[i].FitsInGroup(ipToCheck))
+            {
+                // Return out of this method, we are done
+                return;
+            }
+        }
+
+        // If it doesnt fit then we have to make a new group object
+        IPGroup newGroup = new IPGroup();
+
+        // Add the new group to the list
+        groups.Add(newGroup);
+
+    }
+}
