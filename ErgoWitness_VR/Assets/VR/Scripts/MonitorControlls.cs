e61e7fb813756e0d9c569ae16fe0814e41a2bff4
Ben Hoffman <bah8892@g.rit.edu>
2017-04-14 12:28:47 -0400

    put in Steam interactino system instead of my own , now I need to test and see if it is what i want

---
+++ ErgoWitness_VR/Assets/VR/Scripts/MonitorControlls.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR;
+
+namespace Valve.VR.InteractionSystem
+{
+
+    public class MonitorControlls : MonoBehaviour
+    {
+        private Coroutine gripHintRoutine;
+
+        // Show some hint for testing purposes
+        public void TestButton( Hand hand )
+        {
+            if(gripHintRoutine != null)
+            {
+                StopCoroutine( gripHintRoutine );
+            }
+
+            gripHintRoutine = StartCoroutine( ShowGripHint( hand ) );
+        }
+
+        private IEnumerator ShowGripHint( Hand hand )
+        {
+            while( true )
+            {
+                ControllerButtonHints.ShowTextHint( hand, EVRButtonId.k_EButton_Grip, "This is a test!!" );
+                yield return new WaitForSeconds( 3.0f );
+                ControllerButtonHints.HideAllTextHints( hand );
+            }
+        }
+    }
+}
