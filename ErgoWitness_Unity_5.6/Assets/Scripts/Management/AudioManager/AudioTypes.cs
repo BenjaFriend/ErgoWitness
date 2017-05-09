9582c47a61135b78b2565b167976a25a926439b2
Ben Hoffman <bah8892@g.rit.edu>
2017-05-09 09:52:46 -0400

    Removed the old 5.6 Project

--- ErgoWitness_Unity_5.6/Assets/Scripts/Management/AudioManager/AudioTypes.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-/// <summary>
-/// The types of audio that we can have with out audio manager. This way
-/// I can play audio from anywhere
-/// </summary>
-public enum _MyAudioTypes { Tcp, Udp, Http, Ssh, NewComputer, Default }
